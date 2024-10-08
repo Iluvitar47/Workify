bd49e8bc4638259efbfa28a5cf2452dbacdfa27f
Mathéo Fontaine <matheoftne>
2024-10-08 16:24:50 +0200

    [ADD] adding server.js

---
+++ job-board-backend/src/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const HttpException = require('./utils/HttpException.utils');
+const errorMiddleware = require('./middlewares/errors.middleware');
+const userRouter = require('./routes/users.route');
+
+// Init express
+const app = express();
+
+// Init environment variables
+dotenv.config();
+
+// Parse request body as JSON
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+// Enale preflight requests
+app.options('*', cors());
+
+// Setting Port
+const PORT = process.env.PORT || 3000;
+
+app.use(`/api/v1/users`, userRouter);
+
+// 404 error
+app.call('*', (req, res, next) => {
+  const err = new HttpException(404, 'Endpoint Not Found');
+  next(err);
+});
+
+// Error middleware
+app.use(errorMiddleware);
+
+// Starting server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
