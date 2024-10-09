a57c0fb5563eb3aaab7b8b8d0f543bd749269a88
Mathéo Fontaine <matheoftne>
2024-10-09 12:49:14 +0200

    [ADD] adding auth.middleware.js

---
+++ job-board-backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,42 @@
+const HttpException = require('../utils/HttpException.utils');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const auth = (...roles) => {
+    return async function (req, res, next) {
+        try {
+            const authHeader = req.headers.authorization;
+            const bearer = 'Bearer ';
+
+            if (!authHeader || !authHeader.startsWith(bearer)) {
+                throw new HttpException(401, 'Access denied. No credentials sent!');
+            }
+
+            const token = authHeader.replace(bearer, '');
+            const secretKey = process.env.SECRET_JWT || "";
+
+            // Verify token
+            const decoded = jwt.verify(token, secretKey);
+            const user = await userGetOneById(decoded.user_id);
+
+            if (!user) {
+                throw new HttpException(401, 'Authentication failed!');
+            }
+
+            const ownerAuthorized = req.params.id === user_id;
+
+            if (!ownerAuthorized && roles.length && !roles.includes(user.role)) {
+                throw new HttpException(401, 'Unauthorized');
+            }
+
+            req.currentUser = user;
+            next();
+        } catch (err) {
+            e.status = 401;
+            next(err);
+        }
+    }
+}
+
+module.exports = auth;
