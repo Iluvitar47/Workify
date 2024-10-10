4e680ddbced7a94b02045824f214ddb120580b01
Mathéo Fontaine <matheoftne>
2024-10-10 11:56:31 +0200

    [FIX] fixing comments

--- job-board-backend/src/routes/user.route.js
+++ job-board-backend/src/routes/user.route.js
@@ -8,11 +8,11 @@ const awaitHandlerFactory = require('../middlewares/awaitHandlerFactory.middlewa
 const { createUserSchema, updateUserSchema, validateLogin } = require('../middlewares/validators/userValidator.middleware');
 
 router.get('/', awaitHandlerFactory(userController.getAllUsers)); // localhost:5558/api/v1/users
-router.get('/id/:id', awaitHandlerFactory(userController.getUserById)); // localhost:5558/api/v1/users/id/1
+router.get('/id/:id', awaitHandlerFactory(userController.getUserById)); // localhost:5558/api/v1/users/id/:id
 router.get('/whoami', auth(), awaitHandlerFactory(userController.getCurrentUser)); // localhost:5558/api/v1/users/whoami
 router.post('/', createUserSchema, awaitHandlerFactory(userController.createUser)); // localhost:5558/api/v1/users
-router.patch('/id/:id', auth(Role.Admin), updateUserSchema, awaitHandlerFactory(userController.updateUser)); // localhost:5558/api/v1/users/id/1
-router.delete('/id/:id', auth(Role.Admin), awaitHandlerFactory(userController.deleteUser)); // localhost:5558/api/v1/users/id/1
+router.patch('/id/:id', auth(Role.Admin), updateUserSchema, awaitHandlerFactory(userController.updateUser)); // localhost:5558/api/v1/users/id/:id
+router.delete('/id/:id', auth(Role.Admin), awaitHandlerFactory(userController.deleteUser)); // localhost:5558/api/v1/users/id/:id
 
 router.post('/login', validateLogin, awaitHandlerFactory(userController.userLogin)); // localhost:5558/api/v1/users/login
 
