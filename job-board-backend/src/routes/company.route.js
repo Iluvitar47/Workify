a1b681a61fc9251c4ebae287d5ed8763ba72d0e0
Mathéo Fontaine <matheoftne>
2024-10-10 11:59:04 +0200

    [ADD] adding company.router.js and adding routes in server.js

---
+++ job-board-backend/src/routes/company.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const companyController = require('../controllers/company.controller');
+const auth = require('../middlewares/auth.middleware');
+const Role = require('../utils/userRoles.utils');
+const awaitHandlerFactory = require('../middlewares/awaitHandlerFactory.middleware');
+
+const { createCompanySchema, updateCompanySchema } = require('../middlewares/validators/companyValidator.middleware');
+
+router.get('/', auth(Role.Admin), awaitHandlerFactory(companyController.getAllCompanies)); // localhost:5558/api/v1/companies
+router.get('/id/:id', auth(Role.Admin), awaitHandlerFactory(companyController.getCompanyById)); // localhost:5558/api/v1/companies/id/:id
+router.post('/', auth(Role.Admin), createCompanySchema, awaitHandlerFactory(companyController.createCompany)); // localhost:5558/api/v1/companies
+router.patch('/id/:id', auth(Role.Admin), updateCompanySchema, awaitHandlerFactory(companyController.updateCompany)); // localhost:5558/api/v1/companies/id/:id
+router.delete('/id/:id', auth(Role.Admin), awaitHandlerFactory(companyController.deleteCompany)); // localhost:5558/api/v1/companies/id/:id
+
+module.exports = router;
