e572e435bc8d4d80c65f0fbe56ea4afed1f8282c
Mathéo Fontaine <matheoftne>
2024-10-11 16:52:20 +0200

    [ADD] adding cookies system

---
+++ job-board-frontend/src/utils/cookies.utils.ts
@@ -0,0 +1,17 @@
+'use server'
+
+import { cookies } from 'next/headers'
+
+export const setCookie = (name: string, value: string) => {
+  cookies().set({
+    name,
+    value,
+    httpOnly: true,
+    path: '/',
+    expires: new Date(Date.now() + 2600 * 36000)
+  })
+}
+
+export const getCookie = (name: string) => {
+  return cookies().get(name)
+}
