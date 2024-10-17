455ce4b983ac4d3fa9942ed3402f61cfb47e9e18
Mathéo Fontaine <matheoftne>
2024-10-17 11:41:11 +0200

    [FIX] fxing page.tsx from signup to match with SignUp*Compoenent

--- job-board-frontend/src/app/signup/page.tsx
+++ job-board-frontend/src/app/signup/page.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import SignUpComponent from '../../components/SignUpComponent';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import SignUpComponent from '../../components/SignUpComponent';
+import SignUpUserComponent from '@/components/SignUpUserComponent';
 
-export default function Singup() {
+export default function Signup() {
+  const [peopleToken, setPeopleToken] = useState<string | null>(null);
+
+  // Function to check the token in localStorage
+  const checkPeopleToken = () => {
+    const people = localStorage.getItem('people');
+    setPeopleToken(people);
+  };
+
+  useEffect(() => {
+    // Initialize the token when the component loads
+    checkPeopleToken();
+
+    // Add an event listener to detect changes in localStorage
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === 'people') {
+        checkPeopleToken();
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
   return (
-    <>
+    <div>
       <Header />
-      <SignUpComponent />
+      {peopleToken ? <SignUpUserComponent /> : <SignUpComponent />}
       <Footer />
-    </>
+    </div>
   );
 }
